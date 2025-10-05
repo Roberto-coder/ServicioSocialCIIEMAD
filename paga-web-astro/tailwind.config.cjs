14121741e56f214ab0af1e3a063e83bd576a4716
Roberto-coder <robinromero3002@gmail.com>
2025-10-04 22:59:05 -0600

    Configuraciones Globales

--- paga-web-astro/tailwind.config.cjs
+++ paga-web-astro/tailwind.config.cjs
@@ -1,16 +1,29 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
-theme: {
-  extend: {
-    colors: {
-      guinda: '#7B1E3B', // IPN
-      verde: '#3B7A57',  // sustentabilidad
-    },
-    fontFamily: {
-      serif: ['Merriweather', 'serif'],
-      sans: ['Inter', 'sans-serif'],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+        // SustentaTerra
+        cafe: "#543E2D",
+        verdeOscuro: "#234224",
+        azul: "#44A3C3",
+        mostaza: "#D5A63B",
+
+        // Laboratorio
+        guinda: "#C72C2F",
+        naranja: "#EF9C1A",
+        turquesa: "#02A2D0",
+        verde: "#60A917",
+        grisAzulado: "#4C5D67",
+        verdeProfundo: "#0B4536",
+        grisOscuro: "#3F424B",
+      },
+      fontFamily: {
+        serif: ["Playfair Display", "serif"], // títulos
+        sans: ["Inter", "sans-serif"], // texto
+      },
     },
   },
-},
   plugins: [],
-}
+};
