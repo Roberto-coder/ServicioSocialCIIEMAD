29b9875d877d1ddfbf7d64fa41c215840a7066cb
Roberto-coder <robinromero3002@gmail.com>
2025-11-11 03:03:53 -0600

    Deploy: Cambios para el deployado en GCP

--- paga-web-astro/astro.config.mjs
+++ paga-web-astro/astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+// Importa el adaptador de Node
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server", // <-- ¡Importante!
+  adapter: node({
+    // <-- ¡Importante!
+    mode: "standalone",
+  }),
 });
