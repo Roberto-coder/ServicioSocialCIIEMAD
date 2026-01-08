53711f72a1b1a7796ce3451b4d32d9372f33ec40
Roberto-coder <robinromero3002@gmail.com>
2026-01-08 11:38:03 -0600

    Cambio en el archivo astro.config.mjs para deploy con cloudflare

--- paga-web-astro/astro.config.mjs
+++ paga-web-astro/astro.config.mjs
@@ -2,15 +2,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-// Importa el adaptador de Node
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server", // <-- ¡Importante!
-  adapter: node({
-    // <-- ¡Importante!
-    mode: "standalone",
-  }),
+  output: "static",
 });
